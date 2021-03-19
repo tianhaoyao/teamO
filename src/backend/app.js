b5f91a40105a9b3e05a9dfb82b84fd689b84f340
ricecustomer <tianhao.yao@mail.utoronto.ca>
2021-03-19 16:35:32 -0400

    implemented database methods

--- src/backend/app.js
+++ src/backend/app.js
@@ -6,13 +6,50 @@ var cors = require('cors')
 const app = express()
 const port = 3001
 const API_KEY = process.env.REACT_APP_TEAMO_API_KEY;
-var bodyParser = require('body-parser')
-const axios = require('axios');
+const MONGO_USER = process.env.MONGO_USER;
+const MONGO_USER_PASSWORD = process.env.MONGO_USER_PASSWORD;
+const bodyParser = require('body-parser')
 
+const axios = require('axios');
+const mongoose = require('mongoose')
+const Players = require('./models/player')
+const MongoClient = require('mongodb').MongoClient;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+app.use(bodyParser.json());
+const dbURI = 'mongodb+srv://' + MONGO_USER + ':' + MONGO_USER_PASSWORD + '@teamocache.t4kdx.mongodb.net/players?retryWrites=true&w=majority'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true } )
+    .then((result) => console.log('db connected'))
+    .catch((err) => console.log(err));
+// const client = new MongoClient(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//     const collection = client.db("players").collection("players");
+//     // perform actions on the collection object
+//     client.close();
+//   });
 let requestcount = 0;
 
 
 app.use(cors())
+
+async function insertPlayer(name, tier, division, cs, kda, dmg, gold, kp, pref1, pref2) {
+    const player = new Players({
+        name: name,
+        tier: tier,
+        division: division,
+        cs: cs,
+        kda: kda,
+        dmg: dmg,
+        gold: gold,
+        kp: kp,
+        pref1: pref1,
+        pref2: pref2
+    })
+    player.save()
+}
 
 async function getAccount(name) {
     try {
@@ -20,10 +57,23 @@ async function getAccount(name) {
         const response = await axios.get(url);
         requestcount++;
         console.log(requestcount);
-        return response.data
+        console.log("getAccount")
+        console.log(response.data)
+
+        const data = {
+            id: response.data.id,
+            accountId: response.data.accountId,
+            name: response.data.name
+        }
+
+        console.log("sending back")
+        console.log(data)
+
+        return data
     } catch (error) {
-      console.error(error);
+        console.error(error);
     }
+    
 }
 
 async function getSummoner(id) {
@@ -32,6 +82,7 @@ async function getSummoner(id) {
         const response = await axios.get(url);
         requestcount++;
         console.log(requestcount);
+        console.log("getSummoner")
         return response.data
     } catch (error) {
       console.error(error);
@@ -44,6 +95,7 @@ async function getGames(accountId) {
         const response = await axios.get(url);
         requestcount++;
         console.log(requestcount);
+        console.log("getGames")
         return response.data
     } catch (error) {
         const url = `https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountId}?queue=400&api_key=${API_KEY}`;
@@ -59,10 +111,26 @@ async function getMatch(matchId) {
         const response = await axios.get(url);
         requestcount++;
         console.log(requestcount);
+        console.log("getMatch")
         return response.data
     } catch (error) {
       console.error(error);
     }
+}
+
+async function getCache(summonerName) {
+    Players.findOne({ name: summonerName }, function(err, document) {
+        if(err) {
+            console.log(err);
+        }
+        if(document) {
+            return data
+        }
+        else {
+            console.log("didnt find")
+            return null
+        }
+    })
 }
 
 app.get('/summonerName/:name', async (req, res) => {
@@ -104,6 +172,66 @@ app.get('/match/:matchId', async (req, res) => {
         console.error(error);
     }
 })
+
+app.get('/cache/:summonerName', async (req, res) => {
+    try{
+        let data = await getCache(req.params.summonerName)
+        if(data) {
+            res.send(data);
+        }
+        else {
+            res.send({});
+        }
+    }
+    catch (error) {
+        console.error(error);
+    }
+})
+
+app.post('/insertcache/', function(req, res){
+    try {
+        let name = req.body.name
+        let tier = req.body.tier
+        let division = req.body.division
+        let cs = parseFloat(req.body.cs)
+        let kda = parseFloat(req.body.kda)
+        let dmg = parseFloat(req.body.dmg)
+        let gold = parseFloat(req.body.gold)
+        let kp = parseFloat(req.body.kp)
+        let pref1 = req.body.pref1
+        let pref2 = req.body.pref2
+
+        insertPlayer(name, tier, division, cs, kda, dmg, gold, kp, pref1, pref2)
+        res.sendStatus(200);
+    }
+    catch(err) {
+        console.log(err)
+    }
+})
+
+app.get('/cache/', async (req, res) => {
+    
+    Players.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+
+})
+
+app.post('/clearcache/', function(req, res){
+    try {
+        Players.deleteMany({}, function(req, res){})
+        res.sendStatus(200);
+    }
+    catch(err) {
+        console.log(err)
+    }
+})
+
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
---
+++ src/backend/models/player.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const playerSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    tier: {
+        type: String,
+        required: true
+    },
+    division: {
+        type: String,
+        required: true
+    },
+    cs: {
+        type: Number,
+        required: true
+    },
+    kda: {
+        type: Number,
+        required: true
+    },
+    dmg: {
+        type: Number,
+        required: true
+    },
+    gold: {
+        type: Number,
+        required: true
+    },
+    kp: {
+        type: Number,
+        required: true
+    },
+    pref1: {
+        type: String,
+        required: true
+    },
+    pref2: {
+        type: String,
+        required: true
+    },
+
+}, { timestamps: true });
+
+const Players = mongoose.model('player', playerSchema);
+
+module.exports = Players;
